df972302a5099f15f850f16e1867d4158e5351d9
Glaucio Sales <glauciosales1995@gmail.com>
2021-09-25 01:38:31 -0300

    ensure return null if email is empty or null

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,33 @@
+import 'package:rgb_pattern/validation/protocols/protocols.dart';
+import 'package:test/test.dart';
+
+class EmailValidation implements FieldValidation{
+
+  final String field;
+
+  EmailValidation(this.field);
+
+  String validate(String value) {
+    return null;
+  }
+}
+
+void main(){
+  EmailValidation sut;
+
+  setUp((){
+    sut = EmailValidation('any_value');
+  });
+
+  test('Should return null if email is empty', () {
+    final error = sut.validate('');
+    
+    expect(error, null);
+  });
+
+  test('Should return null if email is null', () {
+    final error = sut.validate(null);
+    
+    expect(error, null);
+  });
+}
